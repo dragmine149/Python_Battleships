9fcbc62ed483571951de4212048f48f6609faa1e
Dragmine <dan@thebanners.uk>
2022-03-15 07:48:09 +0000

    Added button functionality for tkInter
    You can now add buttons to your tk inter screen at a certain location.

--- Ui.py
+++ Ui.py
@@ -4,14 +4,18 @@ import tkinter as tk
 class UClass:
     def __init__(self):
         self.canvas = tk.Tk("Battleships", "main")
-        self.canvas.mainloop()
+        self.canvas.title("Battleships")
 
     def screen(self):
         print("Creating screen")
 
-    def button(self):
-        print("Creating button")
+    def button(self, message, command, grid):
+        button = tk.Button(self.canvas, text=message, command=command)
+        button.grid(column=grid[0], row=grid[1])
+        return button
 
 
 if __name__ == "__main__":
     c = UClass()
+    c.button("Quit", c.canvas.destroy, [0, 0])
+    c.canvas.mainloop()  # mainloop has to come at the end.
