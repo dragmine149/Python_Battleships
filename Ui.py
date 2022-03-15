f1a1c4fe6a7029919de684abb6320eefefe8222b
Dragmine <dan@thebanners.uk>
2022-03-15 00:28:48 +0000

    Started work on adding tkinter support

--- Ui.py
+++ Ui.py
@@ -1,1 +1,17 @@
 import tkinter as tk
+
+
+class UClass:
+    def __init__(self):
+        self.canvas = tk.Tk("Battleships", "main")
+        self.canvas.mainloop()
+
+    def screen(self):
+        print("Creating screen")
+
+    def button(self):
+        print("Creating button")
+
+
+if __name__ == "__main__":
+    c = UClass()
