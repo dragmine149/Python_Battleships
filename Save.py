144cc56de93d0118654a6f7471577d87f5ce0127
dragmine149-Termux <dan@thebanners.uk>
2022-04-14 14:51:59 +0100

    Fixed an issue with the 'dir' value in .ListFolder()

--- Save.py
+++ Save.py
@@ -231,7 +231,7 @@ class save:
     """
     def ListDirectory(self, api=True, dir=False):
         if self.api is not None and api is True:
-            return self.api.ListFolder(dir)
+            return self.api.ListFolder(dir=dir)
         else:
             if not dir:
                 try:
