bf5a9d4fde3f986090b2827e2a54dc73f1d3768f
Dragmine <dan@thebanners.uk>
2022-05-05 20:43:56 +0100

    Continued on writing test for new switch class
    - Added test function
    - Added SwitchTest function
    
    SwitchTest
    - Adds 10 -> 20 values to a table and saves it to switch
    - Adds 1 more for error testing
    - Loops through that table and gets the result
    - Checks the result with the original list to check if the data is the same or correct.
    - Return correct values depending on return list input output thing.

--- test_Switch.py
+++ test_Switch.py
@@ -1,8 +1,50 @@
 import Switch
 import random
+import string
+
+
+# Test function to test the function ability of the script
+def FuncTest():
+    print("A")
+    return "ABCDE"
 
 
 # run a series of tests, like a lot
-def test_Switch():
+def SwitchTest():
+
+    # Setup the data
     data = []
-    for i
+    for i in range(10):
+        data.append(random.choice(string.ascii_letters) for _ in range(10))
+        if random.randint(1, 3) == 1:
+            data.append(FuncTest)
+    Switch.data = data
+
+    # Adds another error value
+    data.append(random.choice(string.ascii_letters) for _ in range(10))
+
+    # Loop through and get results
+    results = []
+    for item in data:
+        results.append(Switch.Call(item))
+
+    # loops through and checks for errors.
+    bad = False
+    badList = []
+    for indexItem in len(range(results)):
+        result = results[indexItem]
+        if results != data[indexItem] and result != "ABCDE":
+            badList.append([data[indexItem], result])  # Excepted, Recieved
+            bad = True
+
+    if bad:
+        print(badList)
+        assert False
+        return False
+    else:
+        assert True
+        return True
+
+
+def test_Switch_STUFF():
+    assert SwitchTest()
