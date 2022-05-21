4cb2203a31ba8606b0e659e10152cea7729e8754
Dragmine <dan@thebanners.uk>
2022-05-21 18:59:04 +0100

    Changed some stuff
    - Fixed atom
    - Added more comments
    - Replaced getting paths to a global value.
    - Replaced trying to find file with new wip function.
    -

--- Program/DriveSetup.py
+++ Program/DriveSetup.py
@@ -3,17 +3,21 @@ import Functions
 import platform
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+
 class Setup:
     def __init__(self, Folder):
         self.Folder = Folder
+        self.dir = os.path.dirname(os.path.realpath(__file__))
+        self.parentdir = os.path.abspath(os.path.join(self.dir, '../'))
 
     '''
     checkForFolder
-    - Checks if the folders required for drive have been made, and if not make them.
+    - Checks if the folders required for drive have been made
+      and if not make them.
     '''
     def checkForFolder(self):
         if not os.path.exists('ApiFiles'):
-            print('Making folder {}/ApiFiles'.format(os.path.dirname(os.path.realpath(__file__))))
+            print('Making folder {}/ApiFiles'.format(self.dir))
             os.mkdir('ApiFiles')
 
     '''
@@ -26,30 +30,32 @@ class Setup:
             if platform.system() == "windows":
                 cmdStart = "move"
 
-            if os.path.exists('credentials.json'): # working
-                cmd = '{} credentials.json ApiFiles'.format(cmdStart)
+            self.levels = 0
+            path = Functions.searchDirectory(self.dir, 'credentials.json')
+
+            if path is not None:
+                cmd = "{} {} ApiFiles".format(cmdStart, path)
                 os.system(cmd)
-                print('Moved file {}/credentials.json!'.format(os.path.dirname(os.path.realpath(__file__))))
-            elif os.path.exists('../credentials.json'):  # checks 1 directory above as well.
-                cmd = "{} ../credentials.json ApiFiles".format(cmdStart)
-                os.system(cmd)
-                print('Moved file {}credentials.json!'.format(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../'))))
+                print('Moved File: {} to {}/ApiFiles'.format(path, self.dir))
             else:
+                print('Failed to find in folders!')
                 File = None
                 while File is None:
-                    File = input('Please enter location of google drive api token: ')  # add better info
+                    File = input('Please enter location of google drive api token: ')  # noqa
                     if os.path.exists(File):
-                        os.system('{} {} ApiFiles/'.format(cmdStart, File))  # fix with windows, add file check
+                        # moves and renames just in case.
+                        os.system('{} {} ApiFiles/credentials.json'.format(cmdStart, File))  # noqa
                     else:
                         Functions.clear(2, "File not found!")
                         File = None
         else:
             print("File 'credentials.json' already found in directory!")
         return True
-    
+
     def main(self):
         self.checkForFolder()
         self.getFile()
+
 
 if __name__ == '__main__':
     Set = Setup('1jgyfEG0R76adWlnyzqDU030ps-mk4M20')
